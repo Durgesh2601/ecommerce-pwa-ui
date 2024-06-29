17fe15990bd171d54d460ac85669ccc3d770f617
Durgesh2607 <dk829445@gmail.com>
2024-06-29 15:16:37 +0530

    fix: nav link

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -29,10 +29,12 @@ const NavigationMenu = () => {
 
   return (
     <nav className="bg-blue-500 text-white p-4 fixed top-0 left-0 w-full z-10 flex justify-between items-center">
-      <div className="flex items-center">
-        <img src={logo} alt="Logo" className="w-8 h-8 mr-2" />
-        <span className="text-xl font-semibold">My E-Commerce</span>
-      </div>
+      <Link to={"/"}>
+        <div className="flex items-center">
+          <img src={logo} alt="Logo" className="w-8 h-8 mr-2" />
+          <span className="text-xl font-semibold">My E-Commerce</span>
+        </div>
+      </Link>
       {/* Hamburger menu icon for small screens */}
       <div className="md:hidden">
         <button
@@ -100,28 +102,28 @@ const NavigationMenu = () => {
           ))}
         </ul>
         <div className="relative">
-        <Link to="/cart" className="flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="size-7"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-            />
-          </svg>
+          <Link to="/cart" className="flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+              />
+            </svg>
 
-          {cartItems.length > 0 && (
-            <span className="absolute cart-count top-0 right-0 bg-red-600 text-white rounded-full px-1.5 text-xs">
-              {cartItems.length > 9 ? "9+" : cartItems.length}
-            </span>
-          )}
-        </Link>
+            {cartItems.length > 0 && (
+              <span className="absolute cart-count top-0 right-0 bg-red-600 text-white rounded-full px-1.5 text-xs">
+                {cartItems.length > 9 ? "9+" : cartItems.length}
+              </span>
+            )}
+          </Link>
         </div>
       </div>
     </nav>
