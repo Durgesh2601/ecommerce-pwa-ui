a5fe720dcd05d5229367d4fe7cf3bfcad35ce198
Durgesh2607 <dk829445@gmail.com>
2024-06-29 11:55:39 +0530

    fix: cart count

--- src/components/NavigationMenu/NavigationMenu.jsx
+++ src/components/NavigationMenu/NavigationMenu.jsx
@@ -99,6 +99,7 @@ const NavigationMenu = () => {
             </li>
           ))}
         </ul>
+        <div className="relative">
         <Link to="/cart" className="flex items-center">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -106,7 +107,7 @@ const NavigationMenu = () => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="w-7 h-7"
+            className="size-7"
           >
             <path
               strokeLinecap="round"
@@ -114,12 +115,14 @@ const NavigationMenu = () => {
               d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
             />
           </svg>
+
           {cartItems.length > 0 && (
-            <span className="ml-1 text-sm text-white bg-red-600 rounded-full px-2 py-0.5">
+            <span className="absolute cart-count top-0 right-0 bg-red-600 text-white rounded-full px-1.5 text-xs">
               {cartItems.length > 9 ? "9+" : cartItems.length}
             </span>
           )}
         </Link>
+        </div>
       </div>
     </nav>
   );
